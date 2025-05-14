a941cf5092276c902260b5a1a16014a058723192
Shubham Bihade <shubhambihade24@gmail.com>
2025-05-14 08:41:02 +0000

    dataTypes Summary

--- my_basics/conversionOperations.js
+++ my_basics/conversionOperations.js
@@ -1,16 +1,53 @@
-// let score = null;
+// let isLoggedIn = "shubham";
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// let someNumber = 588;
 
-// let valueInNumber = Number(score);
+// let stringNumber = String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof (stringNumber));
 
-// console.log(valueInNumber);
 
-let isLoggedIn = "shubham";
-let booleanIsLoggedIn = Boolean(isLoggedIn);
-let someNumber = 588;
+// **********************Operations****************************//
 
-let stringNumber = String(someNumber);
 
-console.log(stringNumber);
+let value = 4;
+let negativeValue = -value;
+// console.log(negativeValue);
 
-console.log(typeof (stringNumber));
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**3);
+// console.log(2%2);
+
+// let str1 = "Shubham";
+// let str2 = " Bihade";
+
+// let strFull = str1 + str2;
+// console.log(strFull);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log((3 + 4) * 5 % 10);
+// console.log(+true);
+// console.log(+"");
+
+// let num1, num2, num3;
+
+// num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+++gameCounter;
+
+console.log(gameCounter);
+
+
+
+
+
+
+
 
---
+++ my_basics/dataTypes-summary.js
@@ -0,0 +1,39 @@
+// Primitive
+
+// 7 Types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100;
+const scoreValue = 100.3;
+
+const isLoggedIn = false;
+const outsideTemp = null;
+
+let userEmail;
+
+const id = Symbol('123')
+
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 3455667788995445654655n;
+
+const heros = ["Shaktiman","Batman","Superman"]
+
+let theObj = {
+    name: "shubham",
+    age: 29,
+    address: "Pune"
+}
+
+const myFunction = function(){
+    console.log("Hello Shubham!");    
+}
+
+console.log(typeof bigNumber);
+
+
+// Reference (Non Primitive)
+
+// Array, Objects, Functions
+
